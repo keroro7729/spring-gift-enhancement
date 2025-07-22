ac1779b4026c6f3bf484d9443b587d40f47385fe
keroro7729 <keroro06108@gmail.com>
2025-07-22 14:58:17 +0900

    feat: 옵션 도메인 엔티티 정의 및 레파지토리 추가

---
+++ src/main/java/gift/domain/product/ProductOption.java
@@ -0,0 +1,57 @@
+package gift.domain.product;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_option")
+public class ProductOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    protected ProductOption() {
+
+    }
+
+    private ProductOption(Long id, String name, Integer quantity, Product product) {
+        this.id = id;
+        validateName(name);
+        this.name = name;
+        validateQuantity(quantity);
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public static ProductOption of(String name, Integer quantity, Product product) {
+        return new ProductOption(null, name, quantity, product);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new ProductOptionException("상품 옵션 이름은 필수값입니다.");
+        }
+        if (!name.matches("^[A-Za-z가-힣0-9()\\[\\]+\\-&/_ ]{1,50}$")) {
+            throw new ProductOptionException("상품 옵션 이름은 50자 이하 한글, 영문, 숫자, 특수문자로 이루어져있어야 합니다.  ( ), [ ], +, -, &, /, _");
+        }
+    }
+
+    private void validateQuantity(Integer quantity) {
+        if (quantity == null) {
+            throw new ProductOptionException("상품 옵션 수량은 필수값입니다.");
+        }
+        if (quantity <= 0 || 100000000 <= quantity) {
+            throw new ProductOptionException("상품 옵션 수량은 최소 1개 이상 1억 개 미만");
+        }
+    }
+}
